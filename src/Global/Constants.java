07b192141672bc420d642dc4199832e1b7f66bdf
whdudgns412 <33975263+whdudgns412@users.noreply.github.com>
2019-06-21 11:32:26 +0900

    0.7.0.1 update
    version name update

--- src/Global/Constants.java
+++ src/Global/Constants.java
@@ -18,7 +18,7 @@ import Utility.Layout;
 import Utility.TriggeredButton;
 
 public class Constants {	
-	public static final String ProgramVersion = "V0.6.1.1 - Beta";
+	public static final String ProgramVersion = "V0.7.0.1 - Beta";
 	public static final long FrameCountPeriod = 850;
 	public static final int FPSLimit = 144;
 	
